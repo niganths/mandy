6e6ccb448f0d46a6f17f714f069cee456b4c6018
niganth <niganth21@gmail.com>
2024-05-14 20:36:17 +0530

    vercel.json

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDatabase = () => {
-    // Disable strictQuery globally for Mongoose
+    
     mongoose.set('strictQuery', false);
 
     mongoose.connect(process.env.DB_LOCAL_URI,{
